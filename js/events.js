0c27ce56ed403e744bfa5fe8ca3d370586738761
Anthea Harvey-Brown <tharveybrown@gmail.com>
2018-07-02 03:24:30 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,15 +1,12 @@
-//define functions here
 function getIt() {
   $('p').on("click", function () {
     alert("Hey!");}
 )}
-  
 function frameIt() {
   $('img').on('load', function() {
     $('img').addClass('tasty');
   })
 }
-  
 function pressIt() {
   $('#typing').on('keydown', function(key) {
     if (key.which === 71) {
